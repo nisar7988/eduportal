0026d78ddbd6e49cbf1ce3f1359c8c2e2ea57675
Nisar Ahmed <119742616+nisar7988@users.noreply.github.com>
2024-09-13 11:08:45 +0530

    Update dbConnection.js

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,10 +2,14 @@ const mysql = require("mysql2");
 
 
 const connection = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'',
-  database:'eduportal'
+  host: `gateway01.ap-southeast-1.prod.aws.tidbcloud.com`,
+  port: "4000", // Constructed host
+  user: "2nk1Gg5ssyNRz36.root", // Public key from environment variable
+  password: "Kwun6QWArjEPdesS", // Private key from environment variable
+  database: "eduportal", // Your database name
+  ssl: {
+    rejectUnauthorized: false, // Enable this if SSL is required
+  },
 })
 
 connection.connect((err)=>{
